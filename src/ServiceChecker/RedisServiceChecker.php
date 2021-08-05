1dbfd21e63076ff80bef5c2df16ee52001488cbd
tg666 <tomasglawaty@icloud.com>
2021-08-05 03:21:43 +0200

    Added `RedisServiceChecker`

---
+++ src/ServiceChecker/RedisServiceChecker.php
@@ -0,0 +1,67 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SixtyEightPublishers\HealthCheck\ServiceChecker;
+
+use Redis;
+use RedisException;
+use SixtyEightPublishers\HealthCheck\Result\ServiceResult;
+use SixtyEightPublishers\HealthCheck\Result\ResultInterface;
+use SixtyEightPublishers\HealthCheck\Exception\HealthCheckException;
+
+final class RedisServiceChecker implements ServiceCheckerInterface
+{
+	protected string $host;
+
+	protected int $port;
+
+	private ?string $auth;
+
+	private string $serviceName;
+
+	/**
+	 * @param string      $host
+	 * @param int         $port
+	 * @param string|NULL $auth
+	 * @param string      $serviceName
+	 */
+	public function __construct(string $host = '127.0.0.1', int $port = 6379, ?string $auth = NULL, string $serviceName = 'redis')
+	{
+		$this->host = $host;
+		$this->port = $port;
+		$this->auth = $auth;
+		$this->serviceName = $serviceName;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function getName(): string
+	{
+		return $this->serviceName;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public function check(): ResultInterface
+	{
+		try {
+			$redis = new Redis();
+
+			$redis->connect($this->host, $this->port);
+
+			if (NULL !== $this->auth && FALSE === $redis->auth($this->auth)) {
+				return ServiceResult::createError($this->getName(), 'unauthorized', new HealthCheckException('Failed to auth a connection.'));
+			}
+
+			$redis->ping();
+			$redis->close();
+
+			return ServiceResult::createOk($this->getName());
+		} catch (RedisException $e) {
+			return ServiceResult::createError($this->getName(), 'down', new HealthCheckException($e->getMessage(), $e->getMessage(), $e));
+		}
+	}
+}
